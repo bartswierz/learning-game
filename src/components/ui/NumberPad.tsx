6b14910d1e5ab8239581efd06731402876251f73
Bart <bartoszswierzynski7@gmail.com>
2024-01-21 19:48:29 -0600

    updated numberpad component logic layout for readability

--- src/components/ui/NumberPad.tsx
+++ src/components/ui/NumberPad.tsx
@@ -1,4 +1,4 @@
-import { FaArrowLeft, FaDivide } from "react-icons/fa6";
+import { FaUndoAlt } from "react-icons/fa";
 
 interface NumberPadProps {
   handleUserInputCallback: (userInput: string) => void;
@@ -8,169 +8,60 @@ interface NumberPadProps {
 
 // Component containing buttons 0-9 and a clear button to reset user input
 const NumberPad = ({ handleUserInputCallback, checkAnswerCallback, userInput }: NumberPadProps) => {
-  // const numberPadValues = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "Clear"];
-  // const numberPadValues = ["+", "-", "x", "/", "7", "8", "9", "[]", "4", "5", "6", "[]", "1", "2", "3", "[]", "0", "Clear"];
-  // const numberPadValues = ["<-", "/", "x", "-", "7", "8", "9", "+", "4", "5", "6", "[]", "1", "2", "3", "[]", "0", ".", "Clear"];
-  // const numberPadValues = ["<-", "/", "x", "-", "7", "8", "9", "+", "4", "5", "6", "[]", "1", "2", "3", "[]", "0", ".", "Clear"];
-  const numberPadValues = [
-    {
-      id: "undo",
-      value: "undo",
-      bgColor: "bg-red-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "divide",
-      value: "/",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "multiply",
-      value: "x",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "subtract",
-      value: "-",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "seven",
-      value: "7",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "eight",
-      value: "8",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "nine",
-      value: "9",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "add",
-      value: "+",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-2",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "four",
-      value: "4",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "five",
-      value: "5",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "six",
-      value: "6",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "one",
-      value: "1",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "two",
-      value: "2",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "three",
-      value: "3",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "enter",
-      value: "=",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-2",
-      colSpan: "col-span-1",
-    },
-    {
-      id: "zero",
-      value: "0",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-2",
-    },
-    {
-      id: "period",
-      value: ".",
-      bgColor: "bg-blue-500",
-      rowSpan: "row-span-1",
-      colSpan: "col-span-1",
-    },
-  ];
-
-  // const disabled: boolean = globals.userInput === "" ? true : false;
-
   // Updates userInput state in the parent component via callback function
   const handleClick = (value: string) => {
-    if (value === "Clear" || value === "check") handleUserInputCallback(""); // RESET INPUT
-    else handleUserInputCallback(userInput + value); // ADD VALUE TO THE END OF THE CURRENT INPUT
+    console.log("user clicked: ", value);
+    // USER CLICKED A NUMBER 0 through 9 - APPEND TO THE USERINPUT STRING AND UPDATE THE STATE
+    if (["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"].includes(value)) handleUserInputCallback(userInput + value);
+    // USER CLICKED UNDO - Remove last character from the userInput string and update the state
+    else if (value === "undo") handleUserInputCallback(userInput.slice(0, userInput.length - 1));
+    // USER CLICKED EQUAL
+    else if (value === "=") checkAnswerCallback();
   };
 
-  const handleCheck = () => {
-    console.log("user clicked check");
-    checkAnswerCallback();
-  };
+  interface ButtonInfo {
+    value: string;
+    reactIcon?: JSX.Element;
+    colSpan?: number;
+    rowSpan?: number;
+  }
 
   const NumberPadButtons = () => {
+    // Numberpad values & option custom icons for a few buttons
+    const buttonInfoList: ButtonInfo[] = [
+      { value: "undo", reactIcon: <FaUndoAlt /> },
+      { value: "/" },
+      { value: "*" },
+      { value: "-" },
+      { value: "7" },
+      { value: "8" },
+      { value: "9" },
+      { value: "+", rowSpan: 2 },
+      { value: "4" },
+      { value: "5" },
+      { value: "6" },
+      { value: "1" },
+      { value: "2" },
+      { value: "3" },
+      { value: "=", rowSpan: 2 },
+      { value: "0", colSpan: 2 },
+      { value: "." },
+    ];
+
     return (
-      <ul className="grid grid-cols-4 grid-rows-4 max-w-[90vw] gap-2 w-[400px]">
-        {numberPadValues.map(({ value, id, bgColor, colSpan, rowSpan }) => {
-          return (
-            // <li key={value}>
-            <li key={id} className={`${colSpan} ${rowSpan} w-full h-full`}>
-              <button
-                onClick={() => handleClick(value)}
-                className={`${bgColor} align-middle w-full h-full bg-blue-500 text-white px-2 py-1 rounded-xl hover:bg-blue-700 transition-color duration-300`}
-              >
-                {value === "undo" ? <FaArrowLeft /> : value}
-              </button>
-            </li>
-          );
-        })}
-        {/* <li className="row-span-2 col-span-2 b">
-        <button
-          onClick={handleCheck}
-          className="w-full bg-blue-500 text-white px-3 py-1 rounded-xl hover:bg-blue-700 transition-color duration-300 align-middle"
-        >
-          =
-        </button>
-      </li> */}
+      <ul className="grid grid-cols-4 max-w-[90vw] gap-2 w-[400px] justify-centerX place-content-centerX">
+        {buttonInfoList.map((item) => (
+          <li
+            key={item.value}
+            // IF 'ROWSPAN' OR 'COLSPAN' EXIST, APPLY TO THE LIST ELEMENT
+            className={`${item.rowSpan && `row-span-${item.rowSpan}`} 
+              ${item.colSpan && `col-span-${item.colSpan}`}`}
+          >
+            <button className="place-self-center place-content-center b w-full h-full p-2" onClick={() => handleClick(item.value)}>
+              {item.reactIcon ? item.reactIcon : item.value}
+            </button>
+          </li>
+        ))}
       </ul>
     );
   };
