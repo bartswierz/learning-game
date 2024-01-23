ec7e6d27d1bacfe46d474882df5e89d219783bce
Bart <bartoszswierzynski7@gmail.com>
2024-01-23 03:32:44 -0600

    updated style of the four home link buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,22 +4,29 @@ import "../globals.css";
 // TODO
 export default function Home() {
   const homepageLinks = [
-    { link: "Addition", bgColor: "bg-green-500 hover:bg-green-600" },
-    { link: "Subtraction", bgColor: "bg-red-500 hover:bg-red-600" },
-    { link: "Multiplication", bgColor: "bg-blue-500 hover:bg-blue-600" },
-    { link: "Division", bgColor: "bg-yellow-500 hover:bg-yellow-600" },
+    {
+      link: "Addition",
+      listItemStyle: "bg-green-500 hover:bg-green-600 hover:shadow-red-500 rounded-tl-[50px]",
+      linkStyle: "rounded-tl-[50px]",
+    },
+    { link: "Subtraction", listItemStyle: "bg-red-500 hover:bg-red-600 rounded-tr-[50px]", linkStyle: "rounded-tr-[50px]" },
+    { link: "Multiplication", listItemStyle: "bg-blue-500 hover:bg-blue-600 rounded-bl-[50px]", linkStyle: "rounded-bl-[50px]" },
+    { link: "Division", listItemStyle: "bg-yellow-500 hover:bg-yellow-600 rounded-br-[50px]", linkStyle: "rounded-br-[50px]" },
   ];
 
   return (
-    <div className="container b mt-8 mb-12">
+    <div className="container mt-8 pb-12">
       <h1 className="text-3xl font-bold text-center mb-4">[placeholder]</h1>
-      <ul className="b grid grid-cols-1 md:grid-cols-2 gap-4">
-        {homepageLinks.map(({ link, bgColor }) => {
+      <ul className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {homepageLinks.map(({ link, listItemStyle, linkStyle }) => {
           return (
-            <li key={link} className={`b flex items-center justify-center h-fullX ${bgColor} h-[20vh] md:h-[30vh] min-h-[100px]`}>
+            <li
+              key={link}
+              className={`flex items-center justify-center h-fullX h-[20vh] md:h-[30vh] min-h-[100px] transition-all duration-300 ${listItemStyle}`}
+            >
               <Link
                 to={`/${link.toLowerCase()}`}
-                className="flex justify-center items-center w-full h-full font-bold text-2xl"
+                className={`flex justify-center items-center w-full h-full font-bold text-2xl transition-all duration-300 shadow-2xl ${linkStyle}`}
                 data-tooltip-target="tooltip-default"
               >
                 {link}
