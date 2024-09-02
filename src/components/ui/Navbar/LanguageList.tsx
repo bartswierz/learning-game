162f40ca1db5ac1666c36bee14bb7b2c27ab7ec9
Bart <bartoszswierzynski7@gmail.com>
2024-09-01 19:07:55 -0500

    update language list component to include flag icons & active language

--- src/components/ui/Navbar/LanguageList.tsx
+++ src/components/ui/Navbar/LanguageList.tsx
@@ -1,17 +1,20 @@
 import { useTranslation } from "react-i18next";
 import useTTSStore from "@/store/tts_store";
+import { US, PL, MX, DE, FR } from "country-flag-icons/react/3x2";
 
+const flagStyle = "w-[33px] h-[22px]";
 const languageList = [
-  { id: "en-US", language: "English" },
-  { id: "es-ES", language: "Spanish" },
-  { id: "pl-PL", language: "Polish" },
-  { id: "fr-FR", language: "French" },
-  { id: "de-DE", language: "German" },
+  { id: "en-US", language: "English", flag: <US className={flagStyle} /> },
+  { id: "es-ES", language: "Spanish", flag: <MX className={flagStyle} /> },
+  { id: "pl-PL", language: "Polish", flag: <PL className={flagStyle} /> },
+  { id: "fr-FR", language: "French", flag: <FR className={flagStyle} /> },
+  { id: "de-DE", language: "German", flag: <DE className={flagStyle} /> },
 ];
 
 const LanguageList = () => {
   const { i18n } = useTranslation();
   const setLanguage = useTTSStore((state) => state.setLanguage);
+  const activeLanguage = useTTSStore((state) => state.language);
 
   const switchLanguage = (languageId: string) => {
     console.log("switching language to: ", languageId);
@@ -22,16 +25,15 @@ const LanguageList = () => {
   return (
     <>
       <ul className="flex flex-col p-4 gap-3 w-[200px]">
-        {languageList.map(({ id, language }) => (
-          <li
-            className="bg-slate-300 hover:bg-blue-500 hover:text-white transition-colors duration-200 rounded-md px-4 py-2 cursor-pointer flex align-items"
-            key={id}
-          >
-            <button onClick={() => switchLanguage(id)}>
-              {/* TODO - add country flags to the button */}
-              {/* <span className="self-center">
-                <EnglishFlagSvg />
-              </span> */}
+        {languageList.map(({ id, language, flag }) => (
+          <li key={id}>
+            <button
+              onClick={() => switchLanguage(id)}
+              className={`${
+                activeLanguage === id ? "bg-blue-500 text-white" : "bg-slate-300"
+              } px-4 py-2 hover:bg-blue-500 hover:text-white transition-colors duration-200 rounded-md cursor-pointer flex align-items w-full gap-2`}
+            >
+              {flag}
               {language}
             </button>
           </li>
