003651f069995ac2345b02693ce276942fa459ed
bartswierz <bartoszswierzynski7@gmail.com>
2024-09-30 21:54:52 -0500

    updated hover and active languages within LanguageList component

--- src/components/ui/Navbar/components/LanguageList.tsx
+++ src/components/ui/Navbar/components/LanguageList.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import useTTSStore from "@/store/tts_store";
 import { US, PL, MX, DE, FR } from "country-flag-icons/react/3x2";
 import { LanguageType } from "@/types/types";
+import { useTheme } from "@/contexts/ThemeContext";
 
 type LanguageListType = {
   id: LanguageType;
@@ -25,6 +26,7 @@ interface LanguageListProps {
 
 const LanguageList = ({ width = "100%" }: LanguageListProps) => {
   const { i18n } = useTranslation();
+  const { theme } = useTheme();
   const setLanguage = useTTSStore((state) => state.setLanguage);
   const activeLanguage = useTTSStore((state) => state.language);
 
@@ -34,15 +36,14 @@ const LanguageList = ({ width = "100%" }: LanguageListProps) => {
   };
 
   return (
-    // <ul className="flex flex-col p-4 gap-3">
     <ul className="flex flex-wrap p-4 gap-3">
       {languageList.map(({ id, language, flag }) => (
         <li key={id} style={{ width }}>
           <button
             onClick={() => switchLanguage(id)}
             className={`${
-              activeLanguage === id ? "bg-blue-500 text-white" : "bg-slate-300"
-            } px-4 py-2 hover:bg-blue-500 hover:text-white transition-colors duration-200 rounded-md cursor-pointer flex align-items w-full gap-2`}
+              activeLanguage === id ? `bg-${theme}-primary text-white` : "bg-slate-300"
+            } px-4 py-2 hover:bg-${theme}-primary hover:text-white transition-colors duration-200 rounded-md cursor-pointer flex align-items w-full gap-2`}
           >
             {flag}
             {language}
