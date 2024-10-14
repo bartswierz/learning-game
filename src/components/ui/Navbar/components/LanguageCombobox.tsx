45e9120a1e1fa1240e376da162c9b2bf57afb384
Bart <bartoszswierzynski7@gmail.com>
2024-10-14 04:23:00 -0500

    added flag icon & cursor pointer to language combobox in mobile navigation menu

--- src/components/ui/Navbar/components/LanguageCombobox.tsx
+++ src/components/ui/Navbar/components/LanguageCombobox.tsx
@@ -3,32 +3,46 @@ import { Check, ChevronsUpDown } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Command, CommandEmpty, CommandGroup, CommandItem, CommandList } from "../../shadcn/command";
 import { Popover, PopoverContent, PopoverTrigger } from "../../shadcn/popover";
+import { US, PL, MX, DE, FR } from "country-flag-icons/react/3x2";
 
 import useTTSStore from "@/store/tts_store";
 import { LanguageType } from "@/types/types";
 import { useTranslation } from "react-i18next";
 import { useTheme } from "@/contexts/ThemeContext";
 
-const languages = [
+type LanguageListType = {
+  languageCode: LanguageType;
+  label: "English" | "Spanish" | "Polish" | "French" | "German";
+  flag: JSX.Element;
+};
+
+const flagStyle = "w-[22px] h-[22px]";
+
+const languages: LanguageListType[] = [
   {
     languageCode: "en-US",
     label: "English",
+    flag: <US className={flagStyle} />,
   },
   {
     languageCode: "es-ES",
     label: "Spanish",
+    flag: <MX className={flagStyle} />,
   },
   {
     languageCode: "pl-PL",
     label: "Polish",
+    flag: <PL className={flagStyle} />,
   },
   {
     languageCode: "fr-FR",
     label: "French",
+    flag: <FR className={flagStyle} />,
   },
   {
     languageCode: "de-DE",
     label: "German",
+    flag: <DE className={flagStyle} />,
   },
 ];
 
@@ -64,13 +78,15 @@ const LanguageCombobox = () => {
           <CommandList>
             <CommandEmpty>No language found.</CommandEmpty>
             <CommandGroup>
-              {languages.map(({ label, languageCode }) => (
+              {languages.map(({ label, languageCode, flag }) => (
                 <CommandItem
                   key={languageCode}
                   value={languageCode}
                   onSelect={(selectedLanguage) => handleOnSelect(selectedLanguage as LanguageType)}
+                  className="cursor-pointer"
                 >
                   <Check className={cn("mr-2 h-4 w-4", ttsLanguage === languageCode ? "opacity-100" : "opacity-0")} />
+                  <span className="mr-2">{flag}</span>
                   {label}
                 </CommandItem>
               ))}
